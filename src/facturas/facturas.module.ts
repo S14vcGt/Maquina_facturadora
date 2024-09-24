59bd7fbf50afb487a58f5080f6ae062ab4aa3e69
Sebastian Mata <sebasmata41@gmail.com>
2024-09-24 13:03:55 -0400

    productos listo

--- src/facturas/facturas.module.ts
+++ src/facturas/facturas.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FacturasService } from './facturas.service';
 import { FacturasController } from './facturas.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Factura } from './entities/factura.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Factura])],
   controllers: [FacturasController],
   providers: [FacturasService],
 })
--- src/productos/productos.module.ts
+++ src/productos/productos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductosService } from './productos.service';
 import { ProductosController } from './productos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Producto } from './entities/producto.entity';
 
 @Module({
+  imports: [ TypeOrmModule.forFeature([Producto])],
   controllers: [ProductosController],
   providers: [ProductosService],
 })
